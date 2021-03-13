f96a35677d2d55fa1f60e05268fc9dfdc3cd0f45
Maciej Zygmanowski <sppmacd@pm.me>
2021-03-13 17:22:43 +0100

    Add stub of TOU page

---
+++ terms.php
@@ -0,0 +1,20 @@
+<?php
+require_once("lib/generator.php");
+$generator = new PCUGenerator("Terms of use");
+$generator->start_content();
+?>
+<h2 id="terms">Terms of use</h2>
+<div class="background-tile">
+    <div class="background-tile-padding">
+        <p>TODO</p>
+    </div>
+</div>
+<h2 id="privacy">Privacy Policy</h2>
+<div class="background-tile">
+    <div class="background-tile-padding">
+        <p>TODO</p>
+    </div>
+</div>
+<?php
+$generator->finish();
+?>
