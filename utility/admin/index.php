27f74a7a73d3d43d07b02cf718ff2807c9582c50
Maciej Zygmanowski <sppmacd@pm.me>
2021-03-14 11:36:31 +0100

    Admin panel: Fix and use PCUGenerator

--- utility/admin/index.php
+++ utility/admin/index.php
@@ -1,47 +1,33 @@
 <?php
 
+require("../../lib/generator.php");
 require("../../lib/pcu.php");
 $userData = pcu_require_role("admin");
 $uid = $userData["id"];
 
+$generator = new PCUGenerator("Admin panel");
+$generator->scripts = ["main.js"];
+$generator->start_content();
+
 ?>
-<html>
-    <head>
-        <title>Admin Panel | PumpkinCell.net</title>
-        <link rel="stylesheet" href="/style.css"/>
-        <meta charset="utf-8"/>
-    </head>
-    <body>
-        <h1><a href="/" class="title-link"><img src="/res/pumpkin2.png" style="height: 50px"/></a><iframe width=395 height=50 style="overflow: hidden; border: none; float: right" src="/u/timer.html?embed=1&mode=3"></iframe></h1>
-        
-        <div id="content">
-            <h2>Admin panel</h2>
-            <div id="controls" class="app-list">
-                <a class="tile" style="width: 20%" href="/phpmyadmin" target="_blank">
-                    <div class='resizable-tile' style="font-size: 15pt">
-                        phpMyAdmin
-                    </div>
-                </a>
-            </div>
-            <input type="text" oninput="updateUserList(this.value)" placeholder="Type to search users..."></input>
-            <div id="data">
-                <div id="user-data" class="background-tile" style="width: calc(100% - 30px); margin: 15px;">
-                </div>
-            </div>
-            <div id="footer">
-                <div id="version-data">
-                </div>
-            </div>
-            <div id="loading">
-            </div>
-        
-            <div id="footer-wrapper">
-                <div id="footer">
-                    <a href="https://github.com/sppmacd">PumpkinCell</a>&nbsp;(c) 2020-2021&nbsp;&#8226;&nbsp;<a href="/terms.php#use">Terms of Use</a>&nbsp;&#8226;&nbsp;<a href="/terms.php#privacy">Privacy Policy</a>
-                </div>
-            </div>
+<h2>Admin panel</h2>
+<div id="controls" class="app-list small">
+    <a is="tlf-button-tile" style="width: 20%" href="/phpmyadmin">phpMyAdmin</a>
+</div>
+<div class="background-tile">
+    <div class="background-tile-padding">
+        <input type="text" oninput="updateUserList(this.value)" placeholder="Type to search users..."></input>
+        <div id="user-data" class="data background-tile">
         </div>
-        
-        <script src="main.js"></script>
-    </body>
-</html>
+    </div>
+</div>
+<div class="background-tile">
+    <div class="background-tile-padding">
+        <div id="version-data">
+        </div>
+    </div>
+</div>
+<div id="loading">
+</div>
+<?php
+$generator->finish();
