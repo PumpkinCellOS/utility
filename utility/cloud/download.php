a6995ff1f25c5482880000853ff59ddb45faf4e4
Maciej Zygmanowski <sppmacd@pm.me>
2021-04-16 15:26:42 +0200

    cloud: Check for download privileges as soon as possible
    The hacker was able to check if specified file exists because
    then there was "Access denied" message, otherwise "File doesn't
    exist".

--- utility/cloud/download.php
+++ utility/cloud/download.php
@@ -2,14 +2,17 @@
 require_once("../../lib/generator.php");
 require_once("../../lib/pcu.php");
 
-pcu_page_type(PCUPageType::Display);
-$userData = pcu_require_login();
-
 if($_SERVER["REQUEST_METHOD"] != "GET")
     pcu_cmd_fatal("Invalid method", 400);
+
+pcu_page_type(PCUPageType::Display);
+$userData = pcu_require_login();
     
 $uid = $_GET["u"];
 $name = basename($_GET["f"]);
+
+if($userData["id"] != $uid)
+    pcu_cmd_fatal("Access denied (" . $userData["id"] . " != $uid)", 403);
 
 if($name == "" || $uid == "")
     pcu_cmd_fatal("Invalid argument");
@@ -19,9 +22,6 @@ $fileName = realpath("files/$uid/$name");
 $exists = stat($fileName);
 if(!$exists)
     pcu_cmd_fatal("File doesn't exist: " . __DIR__ . " . $fileName", 404);
-
-if($userData["id"] != $uid)
-    pcu_cmd_fatal("Access denied (" . $userData["id"] . " != $uid)", 403);
     
 pcu_page_type(mime_content_type("files/$uid/$name"));
 echo file_get_contents("files/$uid/$name");
