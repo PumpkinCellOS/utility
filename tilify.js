7fe0652e8392842fb06f3a193ea35f951513dc24
Maciej Zygmanowski <sppmacd@pm.me>
2021-04-16 13:34:29 +0200

    tilify: check for null instead of empty string in bgtile

--- tilify.js
+++ tilify.js
@@ -38,7 +38,7 @@ class TlfBackgroundTile extends HTMLElement {
     constructor() {
         super();
         var paddingClass = "background-tile-padding";
-        if(this.getAttribute("padding") != "")
+        if(this.getAttribute("padding") != null)
             paddingClass = "background-tile-" + this.getAttribute("padding") + "-padding";
         var inner = this.innerHTML;
         this.innerHTML = `<div class='${paddingClass}'>` + inner + "</div>"
