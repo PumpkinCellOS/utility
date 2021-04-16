fc56329d0fa6c50d5ce2b540dd3e07d5d4b40d45
Maciej Zygmanowski <sppmacd@pm.me>
2021-04-16 15:29:23 +0200

    cloud: Actually use 'fileName' path in download.php

--- utility/cloud/download.php
+++ utility/cloud/download.php
@@ -17,13 +17,13 @@ if($userData["id"] != $uid)
 if($name == "" || $uid == "")
     pcu_cmd_fatal("Invalid argument");
 
-$fileName = realpath("files/$uid/$name");
+$fileName = "files/$uid/$name";
 
 $exists = stat($fileName);
 if(!$exists)
-    pcu_cmd_fatal("File doesn't exist: $fileName", 404);
+    pcu_cmd_fatal("File doesn't exist: $name", 404);
     
-pcu_page_type(mime_content_type("files/$uid/$name"));
-echo file_get_contents("files/$uid/$name");
+pcu_page_type(mime_content_type("$fileName"));
+echo file_get_contents("$fileName");
 
 ?>
