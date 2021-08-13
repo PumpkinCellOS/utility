9e9119dc60427840eca0366cba6f280e60419169
Maciej Zygmanowski <sppmacd@pm.me>
2021-08-13 10:52:38 +0200

    cloud: Fix downloading of files in directories
    The name was a basename of input, which didn't allow to download files
    being lower in the directory tree. Now we just
    validate_path().

--- pcu/utility/cloud/download.php
+++ pcu/utility/cloud/download.php
@@ -1,8 +1,7 @@
 <?php
 require_once("../../lib/generator.php");
 require_once("../../lib/pcu.php");
-
-$PCU_CLOUD = "/var/pcu-cloud";
+require_once("util.php");
 
 if($_SERVER["REQUEST_METHOD"] != "GET")
     pcu_cmd_fatal("Invalid method", 400);
@@ -16,7 +15,8 @@ if(!pcu_is_logged_in())
 }
 
 $uid = $_GET["u"];
-$name = basename($_GET["f"]);
+$name = $_GET["f"];
+validate_path($name);
 
 if($name == "" || $uid == "")
     pcu_cmd_fatal("Invalid argument");
@@ -47,8 +47,9 @@ if(!$exists)
     pcu_cmd_fatal("File doesn't exist: $name", 404);
     
 $fileSize = filesize($fileName);
+$basename = basename($name);
     
-header("Content-Disposition: inline; filename=\"${name}\"");
+header("Content-Disposition: inline; filename=\"${basename}\"");
 header("Content-Length: ${fileSize}");
 pcu_page_type(mime_content_type($fileName));
 
