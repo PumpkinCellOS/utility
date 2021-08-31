ebb08bce227bbafefaa6d03f12e30621a825ed87
Maciej <sppmacd@pm.me>
2021-08-31 15:17:06 +0200

    apache: Make it even more generic

--- install-apache.sh
+++ install-apache.sh
@@ -13,4 +13,3 @@ echo Filling variable fields
 # TODO: Are there some variables in apache config files? maybe use them if so.
 sudo sed -i.bak "s#\\\${PCU_BUILD_DIR}#$PCU_BUILD_DIR#" /etc/apache2/sites-available/pcu-devel.conf
 sudo sed -i.bak "s#\\\${PCU_DEPLOY_DIR}#$PCU_DEPLOY_DIR#" /etc/apache2/sites-available/pcu-prod-le-ssl.conf
-sudo sed -i.bak "s#\\\${PCU_DEPLOY_DIR}#$PCU_DEPLOY_DIR#" /etc/apache2/sites-available/pcu-prod-ssl.conf
