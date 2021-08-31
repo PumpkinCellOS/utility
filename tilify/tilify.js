ed39df76f8e9114504c61efd27eaef963b113b44
Maciej <sppmacd@pm.me>
2021-08-31 15:32:03 +0200

    tilify: Add tlfGetURLParam()
    This is copied from timer.html

--- tilify/tilify.js
+++ tilify/tilify.js
@@ -534,3 +534,10 @@ class TlfDataTable
         return dataTable;
     }
 }
+
+function tlfGetURLParam(name)
+{
+    var url = new URL(window.location);
+    var v = url.searchParams.get(name);
+    return (v === undefined || v === null) ? "" : v;
+}
