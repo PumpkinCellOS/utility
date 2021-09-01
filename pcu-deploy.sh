ca4956da152bbf3b8f71e8a3a87b7aedba230f30
Maciej <sppmacd@pm.me>
2021-09-01 17:50:24 +0200

    Deploy: Do not require sudo

--- pcu-deploy.sh
+++ pcu-deploy.sh
@@ -7,10 +7,10 @@ set -e
 . ./pcu-generate-devel.sh
 
 echo Clearing old build...
-sudo rm -r build-prod
+rm -r build-prod
 
 echo Copying files...
-sudo mkdir build-prod
-sudo cp -prT build build-prod
+mkdir build-prod
+cp -prT build build-prod
 
 echo Successfully deployed to $PCU_DEPLOY_DIR.
