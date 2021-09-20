6b04637ef1a705a1197183579ca281511165c203
Maciej <sppmacd@pm.me>
2021-09-20 19:31:35 +0200

    DSD Editor: Add basic file selection :)

--- pcu/utility/dsd-editor/index.php
+++ pcu/utility/dsd-editor/index.php
@@ -5,9 +5,12 @@ $generator->scripts = ["app.js"];
 $generator->start_content();
 ?>
 <div class="small app-list">
+    <a is="tlf-button-tile" style="width: 16.666%" onclick="selectFile()">Open</a>
     <a is="tlf-button-tile" style="width: 16.666%" onclick="save()">Save</a>
 </div>
 <tlf-background-tile>
+    <!-- TODO: Turn it into a tab view -->
+    <div id="filename-box"></div>
     <textarea id="editor"></textarea>
 </tlf-background-tile>
 <?php
--- pcu/utility/dsd-editor/main.js
+++ pcu/utility/dsd-editor/main.js
@@ -8,15 +8,35 @@ const api = new TlfAPI({
 
 window.api = api;
 
-// TODO: Do not hardcode file name
+let g_currentFile;
+
 window.save = function() {
-    api.put("upload-file", document.getElementById("editor").value, {name: "lesson-data.json"}, function() {
-        tlfNotification("Successfully saved lesson-data.json");
+    api.put("upload-file", document.getElementById("editor").value, {name: g_currentFile}, function() {
+        tlfNotification("Successfully saved " + g_currentFile);
     }, function(data) {
         tlfNotification(data.message, TlfNotificationType.Error);
     })
 }
 
-api.call("download-file", {name: "lesson-data.json"}, function(data) {
-    document.getElementById("editor").value = data;
-})
+function reload(fileName)
+{
+    g_currentFile = fileName;
+    document.getElementById("filename-box").innerText = g_currentFile;
+    api.call("download-file", {name: g_currentFile}, function(data) {
+        document.getElementById("editor").value = data;
+    }, function(data) {
+        tlfNotification(data.message, TlfNotificationType.Warning);
+        document.getElementById("editor").value = "";
+    });
+}
+
+window.selectFile = function() {
+    tlfOpenForm([{type: "select", value: g_currentFile, name: "file", options: [
+        {displayName: "Lesson data", value: "lesson-data.json"},
+        {displayName: "Notes", value: "notes.txt"},
+    ]}], function(data) {
+        reload(data.file);
+    }, {title: "Select file to display"});
+}
+
+reload("lesson-data.json");
