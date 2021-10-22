4da5475ce7f5b35f98be0b1cee5634e76af52a6d
Maciej <sppmacd@pm.me>
2021-10-22 15:49:40 +0200

    Tilify: Rework a way Data Tables work
    The columns are now entirely defined by user, which get the whole
    entry (not only a single property) to generate it.
    
    This commit adds also a separate type of "column" - "control". This is
    basically a button (only them are allowed from widgets for now) which
    calls a callback with a corresponding entry when clicked.

--- tilify/dataTable.js
+++ tilify/dataTable.js
@@ -4,14 +4,19 @@ class TlfDataTable
     {
         this.entries = [];
         this.fields = [];
+        this.controls = [];
     }
 
     // property: string, the property used in `entry` argument to `addEntry()`.
     // header: string, the header used in <thead>
     // generator: Function(data: object): Element, the entry DOM generator.
-    addField(property, header, generator)
+    addField(header, generator)
     {
-        this.fields.push({property: property, header: header, generator: generator});
+        this.fields.push({header: header, generator: generator});
+    }
+    addControl(label, callback, predicate)
+    {
+        this.controls.push({label: label, callback: callback, predicate: predicate});
     }
     addEntry(entry)
     {
@@ -32,20 +37,32 @@ class TlfDataTable
             thead.appendChild(td);
         }
         var tbody = document.createElement("tbody");
-        for(var entry of this.entries)
+        for(const entry of this.entries)
         {
             var tr = document.createElement("tr");
-            for(var field of this.fields)
+            for(const field of this.fields)
             {
                 var td = document.createElement("td");
                 td.appendChild(field.generator.call({
                     text(str) { return document.createTextNode(str); },
                     div(str) { var element = document.createElement("div"); element.innerHTML = str; return element; },
                     span(str) { var element = document.createElement("span"); element.innerHTML = str; return element; },
-                    anchor(label, href) { var element = document.createElement("a"); element.href = href; element.innerHTML = label; return element; }
-                }, [entry[field.property]]));
+                    anchor(label, href) { var element = document.createElement("a"); element.href = href; element.innerHTML = label; return element; },
+                    button(label, onclick) { var element = document.createElement("button"); element.innerHTML = label; element.onclick = onclick; return element;  }
+                }, entry));
                 tr.appendChild(td);
             }
+            let tdControls = document.createElement("td");
+            for(const control of this.controls)
+            {
+                var button = document.createElement("button");
+                if(control.predicate && !control.predicate(entry))
+                    continue;
+                button.onclick = function() { control.callback(entry) };
+                button.innerHTML = control.label;
+                tdControls.appendChild(button);
+            }
+            tr.appendChild(tdControls);
             tbody.appendChild(tr);
         }
 
