6629b240e2bd891d24677a351f4ec06f07af0a0c
Maciej <sppmacd@pm.me>
2021-10-23 16:57:44 +0200

    Dev: Fix dead links

--- pcu/dev.php
+++ pcu/dev.php
@@ -4,11 +4,11 @@ $generator = new PCUGenerator("Development");
 $generator->start_content();
 ?>
 <div class="small app-list">
-    <a is="tlf-button-tile" style="width: 16.666%" href="/template.php">Template
-    </a><a is="tlf-button-tile" style="width: 16.666%" href="/tilify-sandbox.php">Tilify Sandbox
-    </a><a is="tlf-button-tile" style="width: 16.666%" href="/serenity-crash.txt">Serenity Crash
-    </a><a is="tlf-button-tile" style="width: 16.666%" href="/serenity-test.html">Serenity Test
-    </a><a is="tlf-button-tile" style="width: 16.666%" href="/serenity-test2.html">Serenity Test2</a>
+    <a is="tlf-button-tile" style="width: 16.666%" href="/pcu/template.php">Template
+    </a><a is="tlf-button-tile" style="width: 16.666%" href="/pcu/tilify-sandbox.php">Tilify Sandbox
+    </a><a is="tlf-button-tile" style="width: 16.666%" href="/pcu/serenity-crash.txt">Serenity Crash
+    </a><a is="tlf-button-tile" style="width: 16.666%" href="/pcu/serenity-test.html">Serenity Test
+    </a><a is="tlf-button-tile" style="width: 16.666%" href="/pcu/serenity-test2.html">Serenity Test2</a>
 </div>
 <?php
 $generator->finish();
